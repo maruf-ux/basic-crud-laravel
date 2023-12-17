a4594ed88662ac776ce520e0ea2baa1945bdcf48
maruf-ux <bafmarufulislam@gmail.com>
2023-12-18 00:33:47 +0600

    blade changes

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,18 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-</head>
-
-<body class="container">
+@extends('layout.layout')
+@section('content')
     <h1>Create a Product</h1>
     <div class="w-50">
         <form action="{{ route('product.store') }}" method="post">
@@ -20,8 +7,7 @@
             @method('post')
             <div class="mb-3">
                 <label for="" class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" aria-describedby="helpId"
-                    placeholder="name" />
+                <input type="text" class="form-control" name="name" aria-describedby="helpId" placeholder="name" />
             </div>
 
             <div class="mb-3">
@@ -31,8 +17,7 @@
 
             <div class="mb-3">
                 <label for="" class="form-label">Price</label>
-                <input type="text" class="form-control" name="price" aria-describedby="helpId"
-                    placeholder="price" />
+                <input type="text" class="form-control" name="price" aria-describedby="helpId" placeholder="price" />
             </div>
 
             <div class="mb-3">
@@ -43,9 +28,4 @@
             <input type="submit" class="form-control w-25 text-primary" value="Submit">
 
         </form>
-
-    </div>
-
-</body>
-
-</html>
+    @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,24 +1,11 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-</head>
-
-<body class="container">
-    <h1>Product</h1>
+@extends('layout.layout')
+@section('content')
+    <div class="d-flex justify-content-between my-2">
+        <h1>Product</h1>
+        <div class="create">
+            <a href="{{ route('product.create') }}"> Create a new product</a>
+        </div>
+    </div>
     <div class="table-responsive">
         <table class="table table-primary">
             <thead>
@@ -54,9 +41,5 @@
                         </td>
                     </tr>
                 @endforeach
-
             </tbody>
-        </table>
-</body>
-
-</html>
+        @endsection
